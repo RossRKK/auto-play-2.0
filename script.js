5ddbaf7421a2aa4ac581f9d8b6e505b36241d320
Ross Kelso <rosskelso@btinternet.com>
2016-08-26 18:45:23 +0100

    Youtube's most popular videos will load when you aren't logged in

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //track whether the user ha logged in
 var loggedIn = false;
+var apikey = "AIzaSyAWhQ7D5jjmH0u95S8_XyoqXQk7Uhts4O8";
 
 var baseURL = "https://accounts.google.com/o/oauth2/auth";
 var clientID = "715393641613-h6qvdb3ev8p7co0mhdh6l02fuf49fgsc.apps.googleusercontent.com";
@@ -29,6 +30,7 @@ if (response != "") {
       } else {
         loggedIn=true;
         $(".login").hide();
+        window.location.href="#";
       }
     }).error(function () {
       //error with the request we probably need a new token
@@ -57,4 +59,13 @@ if (loggedIn) {
   //pull the users videos
 } else {
   //pull popular videos
+  url = "https://www.googleapis.com/youtube/v3/videos?part=player&chart=mostPopular&key=" + apikey;
+  $.getJSON(url, function (data) {
+    items = data.items;
+    for (var i = 0; i < items.length; i++) {
+      html = "<div class='video'>" + items[i].player.embedHtml + "</div>";
+      console.log(html)
+      $(html).appendTo($("#videos"));
+    }
+  });
 }
